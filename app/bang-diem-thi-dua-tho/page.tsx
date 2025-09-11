2b47675a356bb58295b2597a46a617fb0ff6c900
Tiofe Mai <74725793+ducanhmai521@users.noreply.github.com>
2025-09-11 21:31:41 +0700

    vercel fix again

---
+++ app/bang-diem-thi-dua-tho/page.tsx
@@ -0,0 +1,22 @@
+
+
+'use client';
+
+import PublicEmulationScoreTable from "@/src/public/PublicEmulationScoreTable";
+
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ReactNode } from "react";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+function ConvexClientProvider({ children }: { children: ReactNode }) {
+  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+}
+
+export default function PublicEmulationScorePage() {
+  return (
+    <ConvexClientProvider>
+      <PublicEmulationScoreTable />
+    </ConvexClientProvider>
+  );
+}
